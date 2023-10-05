cf2bd99f2666021af7cef626affe18830beff09f
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-10-05 15:40:11 +0100

    Used List from typing instead of list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,9 +3,10 @@
 This module contains the definition of 'sum_list' a type-annotated function
 that accepts a list of floats and returns their sum
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Returns the list of floats in the 'input_list' argument
     """
