e129b37fbce5887f323be9f8e775efa6277d1dbf
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-10-05 14:43:59 +0100

    Type annotated function to return the sum of two float parameters

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Definition of add which returns the sum of the two float argument passed
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of float a and b
+    """
+    return a + b
