1ee82ebd081a9858df4adadd13d3cb7bc3daf8bf
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-10-06 06:03:25 +0100

    Removed documentations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-"""
-This method contains the definition for safely_get_value which is a
-type-annotated function
-"""
 from typing import Dict, TypeVar, Any, Union
 
 
@@ -10,10 +6,6 @@ T = TypeVar('T')
 
 
 def safely_get_value(dct: Dict, key: Any, default: T) -> Union[Any, T]:
-    """
-    Returns an object in the dictionary that maps to the specified key, else
-    return the specified default or None
-    """
     if key in dct:
         return dct[key]
     else:
