7db1ed63ad6ccef4948b2420a17d4bf8e018279b
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-10-06 13:29:58 +0100

    Replaced 'Dict' with 'Mapping' to specify that we won't mutate the dictionary

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
-from typing import Dict, TypeVar, Any, Union
+"""
+This module contains the definition for 'safely_get_values' which safely
+retieves the value in the dictionary that maps to the specified key if it
+exists, else it returns the specified default
+"""
+from typing import Mapping, TypeVar, Any, Union
 
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Dict, key: Any, default: T) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None]) -> Union[Any, T]:
+    """
+    Return value mapping t okey if it exists, else return default
+    """
     if key in dct:
         return dct[key]
     else:
